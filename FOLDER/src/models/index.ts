f434699400f4975173ff9ab0f8d6df2a0d47902c
Tyler Clark <mtclark0518@gmail.com>
2017-09-12 10:24:32 -0600

    creates and configures an empty database

--- FOLDER/src/models/index.ts
+++ FOLDER/src/models/index.ts
@@ -1,7 +1,11 @@
 import * as Sequelize from 'sequelize'
 
-const sequelize = new Sequelize( process.env.DATABASE_URL || 'postgress://TheTDrive@localhost:5432/project4' )
+const sequelize = new Sequelize( process.env.DATABASE_URL || 'postgres://TheTDrive@localhost:5432/project4' )
 
 
 const db = <any> {}
+
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
 export { db }
