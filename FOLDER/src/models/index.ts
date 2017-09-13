205eda6819b2f133b809c0d4b5d223816bb32e0f
Tyler Clark <mtclark0518@gmail.com>
2017-09-13 11:17:36 -0600

    adds top level model relationships to index.ts

--- FOLDER/src/models/index.ts
+++ FOLDER/src/models/index.ts
@@ -7,29 +7,10 @@ const sequelize = new Sequelize( process.env.DATABASE_URL || 'postgres://TheTDri
 // import models as sequelize models
 //----------_------------------_-------------_----_---------
 const Center = sequelize.import('./center')
-// center has zero or many users
-// center has one and only one director (v0.0.0)
-
 const Director = sequelize.import('./director')
-// director belongs to one center
-// director has many teachers
-// director has many classrooms
-
 const Teacher = sequelize.import('./teacher')
-// teacher belongs to one CLASSROOM (at a time)
-// teacher belongs to one and only one director
-// teacher belongs to one and only one center
-
 const Classroom = sequelize.import('./classroom')
-// classroom has zero to many children
-// classroom has zero to many teachers
-// classroom belongs to one and only one director
-// classroom belongs to one and only one center
-
-const Children = sequelize.import('./children')
-// student has one and only one family
-// student has one assigned classroom (at a time)
-
+const Student = sequelize.import('./student')
 const Family = sequelize.import('./family')
 // family has one or two parents
 // family has one or many students
@@ -38,14 +19,25 @@ const Family = sequelize.import('./family')
 //----------_------------------_-------------_----_---------
 // define model relatioships
 //----------_------------------_-------------_----_---------
-//
-// FAMILY has one or many STUDENTS
-// STUDENT has one and only one FAMILY
-// DIRECTOR 'EXTENDS?' USER
-// CLASSROOM has zero or many STUDENTS
-// CLASSROOM 
 
 
+//Center.hasOne(Director, {})
+//Center.hasMany(Classroom, {})
+
+//Director.belongsTo(Center)
+//Director.hasMany(Teacher)
+
+//Teacher.belongsTo(Director)
+//Teacher.belongsToMany(Classroom)
+
+//Classroom.belongsTo(Center)
+//Classroom.hasMany(Teacher)
+//Classroom.hasMany(Student)
+
+//Student.belongsToMany(Classroom)
+//Student.belongsTo(Family)
+
+//Family.hasMany(Student)
 
 
 // wrap up our models into a variable
@@ -55,7 +47,7 @@ db.models = {
     Director,
     // Classroom,
     Teacher,
-    // Children,
+    // Student,
     // Family,
 }
 
