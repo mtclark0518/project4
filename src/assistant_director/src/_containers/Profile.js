2b4f3155b5feeeff7bfcfda90ac389b7f14d603e
Tyler Clark <mtclark0518@gmail.com>
2017-09-15 10:45:52 -0600

    adds note

--- src/assistant_director/src/_containers/Profile.js
+++ src/assistant_director/src/_containers/Profile.js
@@ -20,7 +20,7 @@ class Profile extends Component {
             <div>
                 <h1>{profile.given_name}</h1>
                 <h1>{profile.family_name}</h1>
-                
+                <h4>this is what comes back from auth0</h4>                
                 <pre>{JSON.stringify(profile, null, 2)}</pre>
             </div>
 
